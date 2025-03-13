9cd7f61ffe2504e4494965fb948180123a5b928b
NrunMaeru <kout.1918@gmail.com>
2025-03-13 10:06:07 +0900

    Apply .gitignore properly

--- android/app/src/main/kotlin/com/example/ebbinghaus_forgetting_curve/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ebbinghaus_forgetting_curve
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/domain/types/type_def.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-// typedef DaysBuilder = Widget Function(int dayIndex);
-typedef MaterialPageBuilder = Widget Function(BuildContext context);
